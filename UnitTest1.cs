835061c83b7f999d9f069a8d07a5cea35109ad98
ourboy <ourboy@gmail.com>
2024-10-22 18:48:33 +0100

    addition of unittest

---
+++ UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BPCalculator;
+
+
+namespace bpUnitTestProject
+{
+	[TestClass]
+	public class Test1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var bloodPressure = new BloodPressure { Systolic = 70, Diastolic = 40 };
+			var actualCategory = bloodPressure.Category;
+			Assert.AreEqual(BPCategory.Low, actualCategory);
+		}
+	}
+}
