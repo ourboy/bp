9902228f26febacb749e1d0534058e4907c3a94b
ourboy <ourboy@gmail.com>
2024-10-22 18:05:44 +0100

    Clean upSystolic and Diastolic

--- BPCalculator/BloodPressure.cs
+++ BPCalculator/BloodPressure.cs
@@ -1,54 +1,74 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 namespace BPCalculator
 {
-    // BP categories
-    public enum BPCategory
+  // BP categories
+  public enum BPCategory
+  {
+      [Display(Name="Low Blood Pressure")] Low,
+      [Display(Name="Ideal Blood Pressure")]  Ideal,
+      [Display(Name="Pre-High Blood Pressure")] PreHigh,
+      [Display(Name="High Blood Pressure")] High
+  };
+
+  public class BloodPressure
+  {
+    public const int SystolicMin = 70;
+    public const int SystolicMax = 190;
+    public const int DiastolicMin = 40;
+    public const int DiastolicMax = 100;
+
+    [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
+    public int Systolic { get; set; }                       // mmHG
+
+    [Range(DiastolicMin, DiastolicMax, ErrorMessage = "Invalid Diastolic Value")]
+    public int Diastolic { get; set; }                      // mmHG
+
+    // calculate BP category
+    public BPCategory Category
     {
-        [Display(Name="Low Blood Pressure")] Low,
-        [Display(Name="Ideal Blood Pressure")]  Ideal,
-        [Display(Name="Pre-High Blood Pressure")] PreHigh,
-        [Display(Name="High Blood Pressure")] High
-    };
-
-    public class BloodPressure
-    {
-        public const int SystolicMin = 70;
-        public const int SystolicMax = 190;
-        public const int DiastolicMin = 40;
-        public const int DiastolicMax = 100;
-
-        [Range(SystolicMin, SystolicMax, ErrorMessage = "Invalid Systolic Value")]
-        public int Systolic { get; set; }                       // mmHG
-
-        [Range(DiastolicMin, DiastolicMax, ErrorMessage = "Invalid Diastolic Value")]
-        public int Diastolic { get; set; }                      // mmHG
-
-        // calculate BP category
-        public BPCategory Category
+      get
+      {
+				if ((Systolic <= 90 ) && (Diastolic <=59))
         {
-            get
-            {
-                // implement as part of project
-                //throw new NotImplementedException("not implemented yet");
-                if (Systolic < 90 || Diastolic < 60)
-                {
-                    return BPCategory.Low;
-                }
-                else if (Systolic <= 120 && Diastolic <= 80)
-                {
-                    return BPCategory.Ideal;
-                }
-                else if (Systolic <= 140 && Diastolic <= 90)
-                {
-                    return BPCategory.PreHigh;
-                }
-                else
-                {
-                    return BPCategory.High;
-                }
-            }
+          return BPCategory.Low;
         }
+
+        else if (Systolic > 90 && Systolic <=120 )
+        {
+          if ( Diastolic >= 60 && Diastolic <= 80)
+          {
+						return BPCategory.Ideal;
+					}
+          else if (Diastolic >= 81 && Diastolic <= 89)
+					{
+						return BPCategory.PreHigh;
+					}
+					else
+					{
+						return BPCategory.High;
+					}
+				}
+
+        else if (Systolic >= 121 && Systolic <= 139)
+        {
+					if (Diastolic >= 81 && Diastolic <= 89)
+					{
+						return BPCategory.PreHigh;
+					}
+					else
+					{
+						return BPCategory.High;
+					}
+				}
+
+        else
+        {
+				  return BPCategory.High;
+        }
+      }
     }
+  }
 }
